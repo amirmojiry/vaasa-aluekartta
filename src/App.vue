a504d89d487ac49ced90b5ffb0d1c60795052a71
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2026-08-16 03:02:08 +0300

    Keep postal selector inside map layer

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
 import SiteFooter from '@/components/SiteFooter.vue'
 import VaasaMap from '@/components/map/VaasaMap.vue'
 import AreaPostalLinks from '@/components/postal/AreaPostalLinks.vue'
-import PostalAreaSelector from '@/components/postal/PostalAreaSelector.vue'
 import PostalCodeDetail from '@/components/postal/PostalCodeDetail.vue'
 import { AREA_BY_SLUG } from '@/config/areas'
 import { useI18n } from '@/i18n'
@@ -60,7 +59,6 @@ const homeHref = buildUrl()
 
     <section class="home-map-section" :aria-label="t('statisticalAreas')">
       <VaasaMap />
-      <PostalAreaSelector />
       <CityStatisticsSummary />
       <CityStatistics />
       <ElectionHistory city />
